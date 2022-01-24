43cd2e0496dd6ffc77ff020f8b5abc0bafcaf53d
Marco Auer <marco@marcoauer.de>
2022-01-24 11:01:27 +0100

    Adjusted CMakeLists to ignore certain warnings

--- clean.sh
+++ clean.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 echo "--------------------- Cleaning Project --------------------"
 
+echo "-- remove directory 'build-win'"
 rm -r build-win
+
+echo "-- remove directory 'build-lin'"
 rm -r build-lin
+
+echo "-- remove directory 'build-mac'"
 rm -r build-mac
 
+echo "-- remove directory 'package'"
 rm -r package
 
