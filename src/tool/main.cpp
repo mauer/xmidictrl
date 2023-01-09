2d0add297ba9a6d6f6a05d479940be66c970d1f5
Michael Hasling <michael.hasling@gmail.com>
2023-01-09 13:56:32 +0000

    Some further refactoring of CMake files. - moved all X-Plane plugin build directives into sub-directory - added empty template files for xmidictrl_tool ready for further development.

---
+++ src/tool/main.cpp
@@ -0,0 +1,21 @@
+//---------------------------------------------------------------------------------------------------------------------
+//   XMidiCtrl - MIDI Controller plugin for X-Plane
+//
+//   Copyright (c) 2023 Michael Hasling
+//
+//   XMidiCtrl is free software: you can redistribute it and/or modify it under the terms of the
+//   GNU Affero General Public License as published by the Free Software Foundation, either version 3
+//   of the License, or (at your option) any later version.
+//
+//   XMidiCtrl is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+//   the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU Affero General Public License for more details.
+//
+//   You should have received a copy of the GNU Affero General Public License along with XMidiCtrl.
+//   If not, see <https://www.gnu.org/licenses/>.
+//---------------------------------------------------------------------------------------------------------------------
+
+int main(int argc, char **argv)
+{
+    return -1;
+}
