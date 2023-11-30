71c57bf0b5c9f2c64addd240292fe001a207bce8
Marco Auer <marco@marcoauer.de>
2023-11-30 18:05:24 +0100

    Revert "Check std::format support across all platforms"
    This reverts commit 3cc7920c2bccce3d7c40043b250e9a6b9e07ce33.

--- build_release/lib/toml11/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/marco/Projects/xmidictrl/lib/toml11
-# Build directory: C:/Users/marco/Projects/xmidictrl/build_release/lib/toml11
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
