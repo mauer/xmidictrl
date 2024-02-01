mauer/xmidictrl

commit 9062e99aba85b777bacb27064f1990b7a77a37f8

Additional cleaning up and refactoring

lib/env/xplane/data_xplane.cpp
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------------------------------------------------------------
 //   XMidiCtrl - MIDI Controller plugin for X-Plane
 //
-//   Copyright (c) 2021-2022 Marco Auer
+//   Copyright (c) 2021-2024 Marco Auer
 //
 //   XMidiCtrl is free software: you can redistribute it and/or modify it under the terms of the
 //   GNU Affero General Public License as published by the Free Software Foundation, either version 3
@@ -31,7 +31,7 @@ namespace xmidictrl {
  */
 bool data_xplane::check(std::string_view in_name)
 {
-    data_item *item = retrieve_data(in_name);
+    const auto item = retrieve_data(in_name);
 
     if (item == nullptr)
         return false;
@@ -47,7 +47,7 @@ bool data_xplane::read(text_logger &in_log, std::string_view in_name, std::strin
 {
     out_value.assign(std::string());
 
-    data_item *item = retrieve_data(in_name);
+    const auto item = retrieve_data(in_name);
 
     if (item == nullptr)
         return false;
@@ -112,7 +112,7 @@ bool data_xplane::read(text_logger &in_log, std::string_view in_name, float &out
 {
     out_value = 0.0f;
 
-    data_item *item = retrieve_data(in_name);
+    const auto item = retrieve_data(in_name);
 
     if (item == nullptr)
         return false;
@@ -166,7 +166,7 @@ bool data_xplane::read(text_logger &in_log, std::string_view in_name, float &out
  */
 bool data_xplane::read(text_logger &in_log, std::string_view in_name, std::vector<float> &out_values)
 {
-    data_item *item = retrieve_data(in_name);
+    const auto item = retrieve_data(in_name);
     out_values = {};
 
     if (item == nullptr)
@@ -188,7 +188,7 @@ bool data_xplane::read(text_logger &in_log, std::string_view in_name, std::vecto
  */
 bool data_xplane::read(text_logger &in_log, std::string_view in_name, std::vector<int> &out_values)
 {
-    data_item *item = retrieve_data(in_name);
+    const auto item = retrieve_data(in_name);
     out_values = {};
 
     if (item == nullptr)
@@ -210,7 +210,7 @@ bool data_xplane::read(text_logger &in_log, std::string_view in_name, std::vecto
  */
 bool data_xplane::write(text_logger &in_log, std::string_view in_name, std::string_view in_value)
 {
-    data_item *item = retrieve_data(in_name);
+    const auto item = retrieve_data(in_name);
 
     if (item == nullptr)
         return false;
@@ -265,7 +265,7 @@ bool data_xplane::write(text_logger &in_log, std::string_view in_name, std::stri
  */
 bool data_xplane::write(text_logger &in_log, std::string_view in_name, float in_value)
 {
-    data_item *item = retrieve_data(in_name);
+    const auto item = retrieve_data(in_name);
 
     if (item == nullptr)
         return false;
@@ -313,7 +313,7 @@ std::string data_xplane::toggle(text_logger &in_log,
                                 std::string_view in_value_on,
                                 std::string_view in_value_off)
 {
-    data_item *item = retrieve_data(in_name);
+    const auto item = retrieve_data(in_name);
 
     if (item == nullptr)
         return {};
@@ -364,8 +364,8 @@ data_item *data_xplane::retrieve_data(std::string_view in_name)
         dataref_name = dataref_name.substr(0, dataref_name.find('['));
     }
 
-    if (m_data_cache.count(dataref_name) == 0) {
-        std::unique_ptr<data_item> item = std::make_unique<data_item>();
+    if (m_data_cache.contains(dataref_name)) {
+        auto item = std::make_unique<data_item>();
 
         item->dataref = XPLMFindDataRef(dataref_name.c_str());
         item->name = dataref_name;
@@ -382,7 +382,7 @@ data_item *data_xplane::retrieve_data(std::string_view in_name)
             item->size = XPLMGetDatab(item->dataref, nullptr, 0, 0);
 
         // add new dataref to cache
-        m_data_cache.emplace(dataref_name, std::move(item));
+        m_data_cache.try_emplace(dataref_name, std::move(item));
     }
 
     return m_data_cache.at(dataref_name).get();
@@ -398,7 +398,7 @@ int data_xplane::get_index(text_logger &in_log, std::string_view in_name)
 
     // is the dataref an array?
     if (in_name.back() == ']') {
-        int pos = static_cast<int>(in_name.find('['));
+        auto pos = static_cast<int>(in_name.find('['));
 
         // read index
         try {

lib/common/midi_logger.cpp
@@ -19,7 +19,6 @@
 
 // Standard
 #include <mutex>
-#include <vector>
 
 namespace xmidictrl {
 
@@ -124,7 +123,7 @@ midi_message* midi_logger::message(int in_index)
 void midi_logger::add(const std::shared_ptr<midi_message>& in_msg)
 {
 	std::mutex mutex;
-	std::lock_guard<std::mutex> lock(mutex);
+	std::scoped_lock lock(mutex);
 
 	if (!m_enabled)
 		return;

lib/map/map_in/map_in_drf.cpp
@@ -235,7 +235,7 @@ std::string map_in_drf::build_mapping_text(bool in_short)
 
 	map_str.append("Values = [");
 
-	for (auto& value: m_values) map_str.append(" '" + value + "', ");
+	for (const auto& value: m_values) map_str.append(" '" + value + "', ");
 
 	map_str.append("]" + sep_str);
 

lib/map/map_in/map_in_label.cpp
@@ -167,7 +167,7 @@ void map_in_label::read_label(text_logger& in_log, toml::value& in_data, toml::v
             for (auto value: values) {
                 std::string value_id = toml_utils::read_string(in_log, value, CFG_KEY_VALUE);
                 std::string value_text = toml_utils::read_string(in_log, value, CFG_KEY_TEXT);
-                m_label->values.emplace(value_id, value_text);
+                m_label->values.try_emplace(value_id, value_text);
             }
         } else {
             in_log.error_line(in_data.location().line(), "Error reading mapping");

lib/map/map_in/map_in_list.cpp
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------------------------------------------------------------
 //   XMidiCtrl - MIDI Controller plugin for X-Plane
 //
-//   Copyright (c) 2021-2023 Marco Auer
+//   Copyright (c) 2021-2024 Marco Auer
 //
 //   XMidiCtrl is free software: you can redistribute it and/or modify it under the terms of the
 //   GNU Affero General Public License as published by the Free Software Foundation, either version 3
@@ -27,22 +27,6 @@
 namespace xmidictrl {
 
 //---------------------------------------------------------------------------------------------------------------------
-//   CONSTRUCTOR / DESTRUCTOR
-//---------------------------------------------------------------------------------------------------------------------
-
-/**
- * Destructor
- */
-map_in_list::~map_in_list()
-{
-    m_list_key.clear();
-    m_list_no.clear();
-}
-
-
-
-
-//---------------------------------------------------------------------------------------------------------------------
 //   PUBLIC
 //---------------------------------------------------------------------------------------------------------------------
 
@@ -50,93 +34,92 @@ map_in_list::~map_in_list()
  * Create the inbound mapping for a device and store it
  */
 void map_in_list::create_mappings(text_logger& in_log,
-                                  toml::array in_profile,
-                                  environment& in_env,
-                                  bool in_is_virtual,
-                                  const device_settings& in_dev_settings,
-                                  std::string_view in_inc_name,
-                                  toml::value& in_config)
+								  toml::array in_profile,
+								  environment& in_env,
+								  bool in_is_virtual,
+								  const device_settings& in_dev_settings,
+								  std::string_view in_inc_name,
+								  toml::value& in_config)
 {
-    std::string log_prefix;
+	std::string log_prefix;
 
-    if (in_is_virtual)
-        log_prefix = "Virtual Device :: ";
-    else
-        log_prefix = "Device " + std::to_string(in_dev_settings.device_no) + " :: ";
+	if (in_is_virtual)
+		log_prefix = "Virtual Device :: ";
+	else
+		log_prefix = "Device " + std::to_string(in_dev_settings.device_no) + " :: ";
 
-    in_log.info(log_prefix + std::to_string(in_profile.size()) + " inbound mapping(s) found");
+	in_log.info(log_prefix + std::to_string(in_profile.size()) + " inbound mapping(s) found");
 
-    // parse each mapping entry
-    for (int map_no = 0; map_no < static_cast<int>(in_profile.size()); map_no++) {
-        std::shared_ptr<map_in> mapping;
+	// parse each mapping entry
+	for (int map_no = 0; map_no < static_cast<int>(in_profile.size()); map_no++) {
+		std::shared_ptr<map_in> mapping;
 
-        in_log.debug(log_prefix + "Read settings for mapping " + std::to_string(map_no));
+		in_log.debug(log_prefix + "Read settings for mapping " + std::to_string(map_no));
 
-        try {
-            auto type = read_map_type(in_log, in_profile[map_no]);
+		try {
+			// depending on the mapping type, we have to read some additional settings
+			switch (read_map_type(in_log, in_profile[map_no])) {
+				using enum map_in_type;
 
-            // depending on the mapping type, we have to read some additional settings
-            switch (type) {
-                case map_in_type::command:
-                    mapping = std::make_shared<map_in_cmd>(in_env);
-                    break;
+				case command:
+					mapping = std::make_shared<map_in_cmd>(in_env);
+					break;
 
-                case map_in_type::dataref:
-                    mapping = std::make_shared<map_in_drf>(in_env);
-                    break;
+				case dataref:
+					mapping = std::make_shared<map_in_drf>(in_env);
+					break;
 
-                case map_in_type::push_pull:
-                    mapping = std::make_shared<map_in_pnp>(in_env);
-                    break;
+				case push_pull:
+					mapping = std::make_shared<map_in_pnp>(in_env);
+					break;
 
-                case map_in_type::encoder:
-                    if (!in_is_virtual)
-                        mapping = std::make_shared<map_in_enc>(in_env, in_dev_settings.default_enc_mode);
-                    else
-                        in_log.error(log_prefix + "Mapping " + std::to_string(map_no)
-                                     + " :: mapping type not supported for virtual devices");
-                    break;
+				case encoder:
+					if (!in_is_virtual)
+						mapping = std::make_shared<map_in_enc>(in_env, in_dev_settings.default_enc_mode);
+					else
+						in_log.error(log_prefix + "Mapping " + std::to_string(map_no)
+									 + " :: mapping type not supported for virtual devices");
+					break;
 
-                case map_in_type::slider:
-                    if (!in_is_virtual)
-                        mapping = std::make_shared<map_in_sld>(in_env);
-                    else
-                        in_log.error(log_prefix + "Mapping " + std::to_string(map_no)
-                                     + " :: mapping type not supported for virtual devices");
-                    break;
+				case slider:
+					if (!in_is_virtual)
+						mapping = std::make_shared<map_in_sld>(in_env);
+					else
+						in_log.error(log_prefix + "Mapping " + std::to_string(map_no)
+									 + " :: mapping type not supported for virtual devices");
+					break;
 
-                case map_in_type::none:
-                    in_log.error(log_prefix + "Mapping " + std::to_string(map_no)
-                                         + " :: Invalid mapping type");
-                    break;
-            }
+				case none:
+					in_log.error(log_prefix + "Mapping " + std::to_string(map_no) + " :: Invalid mapping type");
+					break;
+			}
 
-            if (mapping == nullptr) {
-                in_log.error("Line " + std::to_string(in_profile[map_no].location().line()) + " :: "
-                                     + in_profile[map_no].location().line_str());
-                in_log.error(" --> Error reading mapping");
-                continue;
-            }
+			if (mapping == nullptr) {
+				in_log.error("Line " + std::to_string(in_profile[map_no].location().line())
+							 + " :: " + in_profile[map_no].location().line_str());
+				in_log.error(" --> Error reading mapping");
+				continue;
+			}
 
-            // read the settings and check if everything we need was defined
-            mapping->read_config(in_log, in_profile[map_no], in_config);
-            mapping->set_include_name(in_inc_name);
+			// read the settings and check if everything we need was defined
+			mapping->read_config(in_log, in_profile[map_no], in_config);
+			mapping->set_include_name(in_inc_name);
 
-            if (mapping->check(in_log, in_dev_settings)) {
-                add(mapping);
-                in_log.debug_line(in_profile[map_no].location().line(), "Mapping added");
-            } else {
-                in_log.error("Line " + std::to_string(in_profile[map_no].location().line()) + " :: "
-                                     + in_profile[map_no].location().line_str());
-                in_log.error(" --> Parameters incomplete or incorrect");
-            }
-        } catch (toml::type_error& error) {
-            in_log.error("Line " + std::to_string(in_profile[map_no].location().line()) + " :: "
-                                 + in_profile[map_no].location().line_str());
-            in_log.error(" --> Error reading config");
-            in_log.error(error.what());
-        }
-    }
+			if (mapping->check(in_log, in_dev_settings)) {
+				add(mapping);
+				in_log.debug_line(in_profile[map_no].location().line(), "Mapping added");
+			} else {
+				in_log.error("Line " + std::to_string(in_profile[map_no].location().line())
+							 + " :: " + in_profile[map_no].location().line_str());
+				in_log.error(" --> Parameters incomplete or incorrect");
+			}
+		} catch (toml::type_error& error) {
+			in_log.error("Line " + std::to_string(in_profile[map_no].location().line())
+						 + " :: " + in_profile[map_no].location().line_str());
+			in_log.error(" --> Error reading config");
+			in_log.error(error.what());
+		}
+	}
 }
 
 
@@ -145,23 +128,22 @@ void map_in_list::create_mappings(text_logger& in_log,
  */
 std::vector<std::shared_ptr<map_in>> map_in_list::find(const std::string& in_key)
 {
-    std::vector<std::shared_ptr<map_in>> result {};
+	std::vector<std::shared_ptr<map_in>> result {};
 
-    auto search_result = m_list_key.equal_range(in_key);
+	auto search_result = m_list_key.equal_range(in_key);
 
-    for (auto it = search_result.first; it != search_result.second; it++)
-        result.push_back(it->second);
+	for (auto it = search_result.first; it != search_result.second; it++) result.push_back(it->second);
 
-    return result;
+	return result;
 }
 
 
 /**
  * Return the number of mapping
  */
-size_t map_in_list::size()
+size_t map_in_list::size() const
 {
-    return m_list_key.size();
+	return m_list_key.size();
 }
 
 
@@ -170,7 +152,7 @@ size_t map_in_list::size()
  */
 std::multimap<unsigned int, std::shared_ptr<map_in>>::iterator map_in_list::begin()
 {
-    return m_list_no.begin();
+	return m_list_no.begin();
 }
 
 
@@ -179,7 +161,7 @@ std::multimap<unsigned int, std::shared_ptr<map_in>>::iterator map_in_list::begi
  */
 std::multimap<unsigned int, std::shared_ptr<map_in>>::iterator map_in_list::end()
 {
-    return m_list_no.end();
+	return m_list_no.end();
 }
 
 
@@ -194,52 +176,53 @@ std::multimap<unsigned int, std::shared_ptr<map_in>>::iterator map_in_list::end(
  */
 map_in_type map_in_list::read_map_type(text_logger& in_log, toml::value& in_params)
 {
-    map_in_type type = map_in_type::none;
+	map_in_type type = map_in_type::none;
 
-    try {
-        // read type
-        if (in_params.contains(c_cfg_type.data())) {
-            std::string type_str {in_params[c_cfg_type.data()].as_string()};
+	try {
+		// read type
+		if (in_params.contains(c_cfg_type.data())) {
+			std::string type_str {in_params[c_cfg_type.data()].as_string()};
 
-            in_log.debug_line(in_params.location().line(), "Parameter type = '" + type_str + "'");
+			in_log.debug_line(in_params.location().line(), "Parameter type = '" + type_str + "'");
 
-            // get the mapping type
-            type = translate_map_type(type_str);
-        } else {
-            in_log.error(
-                "Line " + std::to_string(in_params.location().line()) + " :: " + in_params.location().line_str());
-            in_log.error(" --> Parameter '" + std::string(c_cfg_type.data()) + "' is missing");
-        }
-    } catch (toml::type_error& error) {
-        in_log.error(
-            "Line " + std::to_string(in_params.location().line()) + " :: " + in_params.location().line_str());
-        in_log.error("Line " + std::to_string(in_params.location().line()) + " :: Error reading mapping");
-        in_log.error(error.what());
-    }
+			// get the mapping type
+			type = translate_map_type(type_str);
+		} else {
+			in_log.error("Line " + std::to_string(in_params.location().line())
+						 + " :: " + in_params.location().line_str());
+			in_log.error(" --> Parameter '" + std::string(c_cfg_type.data()) + "' is missing");
+		}
+	} catch (toml::type_error& error) {
+		in_log.error("Line " + std::to_string(in_params.location().line()) + " :: " + in_params.location().line_str());
+		in_log.error("Line " + std::to_string(in_params.location().line()) + " :: Error reading mapping");
+		in_log.error(error.what());
+	}
 
-    return type;
+	return type;
 }
 
 
 /**
  * Translate the type string for inbound mappings to an enum value
  */
-map_in_type map_in_list::translate_map_type(std::string_view in_type_str)
+map_in_type map_in_list::translate_map_type(std::string_view in_type_str) const
 {
-    map_in_type type = map_in_type::none;
+	using enum map_in_type;
 
-    if (in_type_str == CFG_MAPTYPE_COMMAND)
-        type = map_in_type::command;
-    else if (in_type_str == CFG_MAPTYPE_SLIDER)
-        type = map_in_type::slider;
-    else if (in_type_str == CFG_MAPTYPE_DATAREF)
-        type = map_in_type::dataref;
-    else if (in_type_str == CFG_MAPTYPE_PUSH_PULL)
-        type = map_in_type::push_pull;
-    else if (in_type_str == CFG_MAPTYPE_ENCODER)
-        type = map_in_type::encoder;
+	map_in_type type = none;
 
-    return type;
+	if (in_type_str == CFG_MAPTYPE_COMMAND)
+		type = command;
+	else if (in_type_str == CFG_MAPTYPE_SLIDER)
+		type = slider;
+	else if (in_type_str == CFG_MAPTYPE_DATAREF)
+		type = dataref;
+	else if (in_type_str == CFG_MAPTYPE_PUSH_PULL)
+		type = push_pull;
+	else if (in_type_str == CFG_MAPTYPE_ENCODER)
+		type = encoder;
+
+	return type;
 }
 
 
@@ -248,11 +231,11 @@ map_in_type map_in_list::translate_map_type(std::string_view in_type_str)
  */
 void map_in_list::add(const std::shared_ptr<map_in>& in_map)
 {
-    m_last_map_no++;
-    in_map->set_no(m_last_map_no);
+	m_last_map_no++;
+	in_map->set_no(m_last_map_no);
 
-    m_list_key.insert({in_map->get_key(), in_map});
-    m_list_no.insert({in_map->no(), in_map});
+	m_list_key.insert({in_map->get_key(), in_map});
+	m_list_no.insert({in_map->no(), in_map});
 }
 
 } // Namespace xmidictrl

lib/map/map_in/map_in_list.h
@@ -36,7 +36,6 @@ namespace xmidictrl {
 class map_in_list {
 public:
     explicit map_in_list() = default;
-    ~map_in_list();
 
     void create_mappings(text_logger& in_log,
                          toml::array in_profile,
@@ -48,14 +47,14 @@ public:
 
     std::vector<std::shared_ptr<map_in>> find(const std::string &in_key);
 
-    size_t size();
+    size_t size() const;
 
     std::multimap<unsigned int, std::shared_ptr<map_in>>::iterator begin();
     std::multimap<unsigned int, std::shared_ptr<map_in>>::iterator end();
 
 private:
     map_in_type read_map_type(text_logger& in_log, toml::value& in_params);
-    map_in_type translate_map_type(std::string_view in_type_str);
+    map_in_type translate_map_type(std::string_view in_type_str) const;
 
     void add(const std::shared_ptr<map_in>& in_map);
 

lib/map/map_out/map_out_list.cpp
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------------------------------------------------------------
 //   XMidiCtrl - MIDI Controller plugin for X-Plane
 //
-//   Copyright (c) 2021-2022 Marco Auer
+//   Copyright (c) 2021-2024 Marco Auer
 //
 //   XMidiCtrl is free software: you can redistribute it and/or modify it under the terms of the
 //   GNU Affero General Public License as published by the Free Software Foundation, either version 3
@@ -23,21 +23,6 @@
 #include "map_out_sld.h"
 
 namespace xmidictrl {
-
-//---------------------------------------------------------------------------------------------------------------------
-//   CONSTRUCTOR / DESTRUCTOR
-//---------------------------------------------------------------------------------------------------------------------
-
-/**
- * Destructor
- */
-map_out_list::~map_out_list()
-{
-    m_list.clear();
-}
-
-
-
 
 //---------------------------------------------------------------------------------------------------------------------
 //   PUBLIC
@@ -131,7 +116,7 @@ map_out_itr map_out_list::end()
 /**
  * Return the number of mapping
  */
-size_t map_out_list::size()
+size_t map_out_list::size() const
 {
     return m_list.size();
 }
@@ -146,7 +131,7 @@ size_t map_out_list::size()
 /**
  * Read the outbound mapping type
  */
-map_out_type map_out_list::read_map_type(text_logger& in_log, toml::value& in_profile)
+map_out_type map_out_list::read_map_type(text_logger& in_log, toml::value& in_profile) const
 {
     map_out_type type = map_out_type::none;
 

lib/map/map_out/map_out_list.h
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------------------------------------------------------------
 //   XMidiCtrl - MIDI Controller plugin for X-Plane
 //
-//   Copyright (c) 2021-2023 Marco Auer
+//   Copyright (c) 2021-2024 Marco Auer
 //
 //   XMidiCtrl is free software: you can redistribute it and/or modify it under the terms of the
 //   GNU Affero General Public License as published by the Free Software Foundation, either version 3
@@ -43,7 +43,6 @@ using map_out_itr = std::vector<std::shared_ptr<map_out>>::iterator;
 class map_out_list {
 public:
     explicit map_out_list() = default;
-    ~map_out_list();
 
     void create_mappings(text_logger& in_log,
                          toml::array in_profile,
@@ -54,10 +53,10 @@ public:
     map_out_itr begin();
     map_out_itr end();
 
-    size_t size();
+    size_t size() const;
 
 private:
-    map_out_type read_map_type(text_logger& in_log, toml::value& in_profile);
+    map_out_type read_map_type(text_logger& in_log, toml::value& in_profile) const;
     static map_out_type translate_map_type(std::string_view in_type_str);
 
     void add(const std::shared_ptr<map_out>& in_map);

commit 010595b6ba160bc8483971f9b56fcba886ebd5d8

Fix start-up crash
9062e99a introduced an inverted condition that resulted in a null
pointer exception.

lib/env/xplane/data_xplane.cpp
@@ -364,7 +364,7 @@ data_item *data_xplane::retrieve_data(std::string_view in_name)
         dataref_name = dataref_name.substr(0, dataref_name.find('['));
     }
 
-    if (m_data_cache.contains(dataref_name)) {
+    if (!m_data_cache.contains(dataref_name)) {
         auto item = std::make_unique<data_item>();
 
         item->dataref = XPLMFindDataRef(dataref_name.c_str());